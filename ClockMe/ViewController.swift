c96d4cff877a9ed4da031e36c26428990ec3a691
Luke Curran <MCurran16@stasaints.net>
2015-03-11 18:41:33 -0500

    Initial Commit

---
+++ ClockMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClockMe
+//
+//  Created by Curran Family Computer on 3/11/15.
+//  Copyright (c) 2015 LukeCurran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
